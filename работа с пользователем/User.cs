71f6c9455d4d1b63832aea4019429cb08e11d921
User <User@WIN-23AB664D43J>
2023-09-23 20:45:38 +0300

    Добавьте файлы проекта.

---
+++ работа с пользователем/User.cs
@@ -0,0 +1,7 @@
+﻿namespace работа_с_пользователем
+{
+    internal class User
+    {
+        internal string Name;
+    }
+}
---
+++ работа с пользователем/Window3.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace работа_с_пользователем
+{
+    /// <summary>
+    /// Логика взаимодействия для Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+
+        private ObservableCollection<User> users = new ObservableCollection<User>();
+        public Window3()
+        {
+            InitializeComponent();
+
+            users.Add(new User() { Name = "Banta Singh" });
+            
+        }
+
+        private void btnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            users.Add(
+        }
+
+        private void btnChangeUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbUsers.SelectedItem != null)
+                (lbUsers.SelectedItem as User).Name = "Smith";
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbUsers.SelectedItem != null)
+                users.Remove(lbUsers.SelectedItem as User);
+        }
+    }
+}
+}
